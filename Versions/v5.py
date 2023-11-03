e7668e4e45f8da21712398981ed3772f8b3ac6f7
Heer Patel <heerpatel1955@gmail.com>
2023-11-03 00:37:51 -0400

    New Code Backups

---
+++ Versions/v5.py
@@ -0,0 +1,52 @@
+from machine import Pin, PWM
+import utime
+
+
+#Front Fin Section
+MIN = 100000
+MID = 900000
+MAX = 1600000
+
+pwm = PWM(Pin(14))
+pwm.freq(50)
+
+pwm1 = PWM(Pin(15))
+pwm1.freq(50)
+
+
+#Rear3#
+#pwm2 = PWM(Pin(13))
+#pwm2.freq(50)
+
+#Rear2#
+#pwm3 = PWM(Pin(12))
+#pwm3.freq(50)
+
+#Rear1#
+#pwm4 = PWM(Pin(11))
+#pwm4.freq(50)
+
+
+while True:
+    utime.sleep(1)
+    pwm.duty_ns(MAX)
+    pwm1.duty_ns(MAX)
+ #   pwm2.duty_ns(MAX)
+#    pwm3.duty_ns(MAX)
+ #   pwm4.duty_ns(MAX)
+    print("max")
+    utime.sleep(1)
+    pwm.duty_ns(MIN)
+    pwm1.duty_ns(MIN)
+ #   pwm2.duty_ns(MIN)
+   # pwm3.duty_ns(MIN)
+   # pwm4.duty_ns(MIN)
+    print("min")
+    utime.sleep(1)
+    
+    pwm.duty_ns(MID)
+    pwm1.duty_ns(MID)
+   # pwm2.duty_ns(MID)
+  #  pwm3.duty_ns(MID)
+  #  pwm4.duty_ns(MID)
+    print("mid")
---
+++ Versions/v7.py
@@ -0,0 +1,57 @@
+from machine import Pin, PWM
+import utime
+
+
+#Front Fin Section
+MIN = 100000
+MID = 900000
+MAX = 1600000
+
+pwm = PWM(Pin(14))
+pwm.freq(50)
+
+pwm1 = PWM(Pin(15))
+pwm1.freq(50)
+
+#Rear Fin Section
+MIN1 = 700000
+MID1 = 900000
+MAX1 = 1200000
+
+
+#Rear3#
+pwm2 = PWM(Pin(13))
+pwm2.freq(50)
+
+#Rear2#
+pwm3 = PWM(Pin(12))
+pwm3.freq(50)
+
+#Rear1#
+pwm4 = PWM(Pin(11))
+pwm4.freq(50)
+
+
+while True:
+    utime.sleep(1)
+    pwm.duty_ns(MAX)
+    pwm1.duty_ns(MAX)
+    pwm2.duty_ns(MAX1)
+    pwm3.duty_ns(MAX1)
+    pwm4.duty_ns(MAX1)
+    print("max")
+    utime.sleep(1)
+    pwm.duty_ns(MIN)
+    pwm1.duty_ns(MIN)
+    pwm2.duty_ns(MIN1)
+    pwm3.duty_ns(MIN1)
+    pwm4.duty_ns(MIN1)
+    print("min")
+    utime.sleep(1)
+    
+    pwm.duty_ns(MID)
+    pwm1.duty_ns(MID)
+    pwm2.duty_ns(MID1)
+    pwm3.duty_ns(MID1)
+    pwm4.duty_ns(MID1)
+    print("mid")
